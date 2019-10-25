3ab03667b52bdf9d803c57cc0bc6b30e86280217
yangyu <yangyu@163.com>
2019-10-25 10:10:14 +0800

    导入test库 解决android.support.test不存在的问题

--- app/src/androidTest/java/com/jzd/jzshop/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jzd/jzshop/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
-package com.goldze.jzshop;
+package com.jzd.jzshop;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.goldze.jzshop", appContext.getPackageName());
+        assertEquals("com.jzd.jzshop", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/jzd/jzshop/ExampleUnitTest.java
+++ app/src/test/java/com/jzd/jzshop/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.goldze.jzshop;
+package com.jzd.jzshop;
 
 import org.junit.Test;
 
